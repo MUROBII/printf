33285c147e11f78dfdb4458d7b8cfdb308315d52
Murobi <murabiusman@gmail.com>
2023-10-18 04:04:44 +0100

    Bypass Deadline

--- printf.c
+++ printf.c
@@ -1,45 +1,61 @@
 #include "main.h"
+
+/* remember to remove the original printf, you used for testing flags[]*/
 /**
- * _printf - format and prints a string stdout (screen)
- * @format: The string specify chars and formats
- *
- * Return: The number of chars printed to stdout
+ * _printf - format and prints to the stdout
+ * @format: the format specifiers
+ * Return: the number of byte printed
  */
 int _printf(const char *format, ...)
 {
-	short index, count, idx;
-	va_list v;/*point to stack*/
-	char buff[BUFFSIZE];/*Buffer to store the resulting string*/
-	/* array of structures that assocites, func to format specifier*/
-	struct chaFun arr_spec[] = {{'c', apchr}, {'s', apstr}, {'\0', NULL}};
+	int byte_count;
+	char *copy;
+	va_list v;
+	flag_t flags[] = {{'-', 0}, {'0', 0}, {'+', 0}, {' ', 0}, {'#', 0},
+			{'\0', 0}};
+/*	short i;*/
+	funct_pointer_t match_func;
 
-	va_start(v, format);/*Initializing pointer to stack*/
-	index  = count = idx = 0;
-	for (; format[index] != '\0'; index++)
-	{/*going through the Array*/
-		if (format[index] == '%')
-		{/*Scanning for format specifier*/
-			index++;
-			if (format[index] == '%')
-			{/*for % as a format specifier*/
-				buff[count] = format[index];
-				count++;
-				continue;
-			}
-
-			for (idx = 0; arr_spec[idx].spec; idx++)
-			{/*Scanning for correlating function*/
-				if (arr_spec[idx].spec == format[index])
-					count += arr_spec[idx].fp(v, buff + count);
+	match_func = NULL;
+	va_start(v, format);
+	copy = (char *) format;
+	if (format == NULL)
+		return (0);
+	for (byte_count = 0; *copy; copy++)
+	{
+		if (*copy == '%')
+		{
+			++copy;
+			match_func = get_req_funct(&(copy), flags);
+			if (match_func) /* if format specifier function exist*/
+				byte_count += match_func(v, flags);
+			else /* what do if format specifier doesn't exist */
+			{
+				write(1, copy, 1);
+				byte_count++;
 			}
 		}
 		else
-		{/*never met format specifier, writing to buffer*/
-			buff[count] = format[index];
-			count++;
+		{
+			write(1, copy, 1);
+			byte_count++;
 		}
 	}
-	write(STDOUT_FILENO, buff, count);
-
-	return (count);
+	/* testing my flags
+	i = 0;
+	while (i <= 5)
+	{
+		printf("%c is %d\n", flags[i].f, flags[i].b);
+		i++;
+	}
+	de_init(flags);
+	i = 0;
+	while (i <= 5)
+	{
+		printf("%c is %d\n", flags[i].f, flags[i].b);
+		i++;
+	}
+	 flag test ends here */
+	va_end(v);
+	return (byte_count);
 }
